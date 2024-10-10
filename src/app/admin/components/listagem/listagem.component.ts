7ddb7edd2ab3507ee00aec9913afdca321c5611f
jeteraguilar <jeteraguilar@gmal.com>
2024-10-09 23:10:42 -0300

    criação serviço funcionário

--- src/app/admin/components/listagem/listagem.component.ts
+++ src/app/admin/components/listagem/listagem.component.ts
@@ -25,7 +25,7 @@ export class ListagemComponent implements OnInit {
   totalLancamentos!: number;
 
   funcionarios!: Funcionario[];
-  @ViewChild(MatSelect, { static: true }) matSelect!: MatSelect;
+  @ViewChild(MatSelect) matSelect!: MatSelect;
   form!: FormGroup;
 
   private pagina!: number;
@@ -38,7 +38,7 @@ export class ListagemComponent implements OnInit {
     private httpUtil: HttpUtilService,
     private snackBar: MatSnackBar,
     private fb: FormBuilder,
-    private funcionarioService: FuncionarioService,
+    private funcionarioService: FuncionarioService
     /*private dialog: MatDialog */
   ) { }
 
@@ -133,51 +133,4 @@ export class ListagemComponent implements OnInit {
     }
     this.exibirLancamentos();
   }
-
-  /* removerDialog(lancamentoId: string) {
-    const dialog = this.dialog.open(ConfirmarDialog, {});
-    dialog.afterClosed().subscribe(remover => {
-      if (remover) {
-        this.remover(lancamentoId);
-      }
-    });
-  } */
-
- /*  remover(lancamentoId: string) {
-    this.lancamentoService.remover(lancamentoId)
-      .subscribe(
-        data => {
-          const msg: string = "Lançamento removido com sucesso!";
-          this.snackBar.open(msg, "Sucesso", { duration: 5000 });
-          this.exibirLancamentos();
-        },
-        err => {
-          let msg: string = "Tente novamente em instantes.";
-          if (err.status == 400) {
-            msg = err.error.errors.join(' ');
-          }
-          this.snackBar.open(msg, "Erro", { duration: 5000 });
-        }
-      );
-  } */
-
 }
-
-/* @Component({
-  selector: 'confirmar-dialog',
-  template: `
-    <h1 mat-dialog-title>Deseja realmente remover o lançamento?</h1>
-    <div mat-dialog-actions>
-      <button mat-button [mat-dialog-close]="false" tabindex="-1">
-        Não
-      </button>
-      <button mat-button [mat-dialog-close]="true" tabindex="2">
-        Sim
-      </button>
-    </div>
-  `,
-})
-export class ConfirmarDialog {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
-}
- */
